a38700432c7786ac33a932b4a8daa97f8c6295b6
Zane Falcao <112317470+falcaozane@users.noreply.github.com>
2023-04-25 09:56:19 +0530

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import image from './amazonvector.png'
 import classes from './App.module.css';
 const Navbar = () => {
   return (
     <header>
         <nav className={classes.Topbar}>
-          {/*<img src="./amazonvector.png" alt="Amazon logo" />*/}
-          <h3 style={{'color': 'white'}}>My Shop</h3>
+          <img src={image} alt="Amazon logo" />
+          {/*<h3 style={{'color': 'white'}}>My Shop</h3>*/}
     </nav>
     </header>
   )
